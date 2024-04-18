6cb4e92dc56485fd9bbf2b99e7758faa5868e1d6
Quang Huy <huynq23091@gmail.com>
2024-04-18 15:31:13 +0700

    Update poker_huy.py

--- poker_huy.py
+++ poker_huy.py
@@ -1,12 +1,15 @@
-RANK = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
-SUIT = ["c", "d", "h", "s"]
+import itertools
+import random
+
+RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+SUITS = ["c", "d", "h", "s"]
 
 def generate_starting_hands():
   starting_hands = []
   for i in range(13):
     for j in range(13):
-      rank1 = RANK[i]
-      rank2 = RANK[j]
+      rank1 = RANKS[i]
+      rank2 = RANKS[j]
       if i == j:
         # generate pair
         starting_hands.append([rank1 + 'h', rank2 + 'd'])
@@ -19,7 +22,7 @@ def generate_starting_hands():
 
 number_of_opponent_clusters = 8
 
-opponent_cluster = [
+opponent_clusters = [
   ["3h2h", "4h2h", "5h2h", "6h2h", "7h2h", "4h3h", "5h3h", "6h3h", "7h3h", "5h4h", "6h4h", "3h2d", "4h2d", "4h3d", "5h2d", "5h3d", "5h4d", "6h2d", "6h3d", "6h4d", "6h5d", "7h2d", "7h3d", "7h4d", "8h2d", "8h3d"],
   ["8h2h", "9h2h", "Th2h", "8h3h", "9h3h", "7h4h", "8h4h", "9h4h", "7h5d", "8h4d", "8h5d", "9h2d", "9h3d", "9h4d", "9h5d", "Th2d", "Th3d", "Th4d", "Th5d", "Jh2d", "Jh3d"],
   ["Th3h", "Th4h", "6h5h", "7h5h", "8h5h", "9h5h", "Th5h", "7h6h", "8h6h", "9h6h", "Th6h", "8h7h", "9h7h", "9h8h", "7h6d", "8h6d", "8h7d", "9h6d", "9h7d", "9h8d", "Th6d", "Th7d", "Th8d"],
@@ -33,7 +36,7 @@ opponent_cluster = [
 all_hands = generate_starting_hands()
 hands_to_cluster = {}
 
-for index, cluster in enumerate(opponent_cluster):
+for index, cluster in enumerate(opponent_clusters):
   for hand in cluster:
     hands_to_cluster[hand] = index + 1
 
@@ -45,3 +48,32 @@ for column_index, column in enumerate(["2", "3", "4", "5", "6", "7", "8", "9", "
       hand = row + 'h' + column + 'd'
     print(hands_to_cluster[hand])
   print("\n")
+
+NUM_SAMPLES = 1000
+
+def precompute_ochs_features():
+  ochs_table = {}
+  for private_card in all_hands:
+    deck = [RANK + SUIT for SUIT in SUITS for RANK in RANKS]
+    deck.remove(private_card[0])
+    deck.remove(private_card[1])
+    public_cards = list(itertools.combinations(deck, 5))
+    for public_card in public_cards:
+      ochs_vector = calculate_ochs(private_card, public_card)
+      ochs_table[(private_card, public_card)] = ochs_vector
+  return ochs_table
+
+def calculate_ochs(private_card, public_card, hand_evaluator, opponent_clusters):
+  ochs_vector = []
+  for cluster in opponent_clusters:
+    wins = 0
+    for _ in range(NUM_SAMPLES):
+      opponent_hand = random.choice(cluster)
+      opponent_final_hand = opponent_hand + ''.join(public_card)
+      if hand_evaluator.evaluate(private_card + ''.join(public_card)) > hand_evaluator.evaluate(opponent_final_hand):
+        wins += 1
+    win_probability = wins / NUM_SAMPLES
+    ochs_vector.append(win_probability)
+  return ochs_vector
+
+precompute_ochs_features()
